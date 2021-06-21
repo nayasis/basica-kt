e24299a1aeebd8541056e7a67834d6359eef3e0d
nayasis <nayasis@gmail.com>
2021-06-21 15:58:16 +0900

    add Platforms

---
+++ src/main/kotlin/com/github/nayasis/kotlin/basica/etc/Platforms.kt
@@ -0,0 +1,40 @@
+package com.github.nayasis.kotlin.basica.etc
+
+class Platforms { companion object {
+
+    val os = Os(
+        name = System.getProperty("os.name").toLowerCase(),
+        architecture = System.getProperty("os.arch"),
+        version = System.getProperty("os.version"),
+        charset = System.getProperty("sun.jnu.encoding"),
+    )
+
+    /** Java Virtual Machine architect  */
+    val jvm = System.getProperty("sun.arch.data.model")
+
+    /** is WINDOWS O/S  */
+    val isWindows = os.name.contains("win")
+
+    /** is LINUX O/S  */
+    val isLinux = os.name.contains("linux")
+
+    /** is UNIX O/S  */
+    val isUnix = os.name.contains("unix")
+
+    /** is SOLARIS O/S  */
+    val isSolaris = os.name.contains("solaris") || os.name.contains("sunos")
+
+    /** is MAC O/S  */
+    val isMac = os.name.contains("mac")
+
+    /** is MAC O/S  */
+    val isAndroid = System.getProperty("java.vm.name") == "Dalvik"
+
+}}
+
+data class Os(
+    val name: String,
+    val architecture: String,
+    val version: String,
+    val charset: String,
+)
