19656c10dd620f34172a506f328530b015a89d25
nayasis <nayasis@gmail.com>
2021-06-23 11:13:33 +0900

    remove observer pattern

--- src/main/kotlin/com/github/nayasis/kotlin/basica/thread/NThreadLocal.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/basica/thread/NThreadLocal.kt
@@ -1,97 +1,59 @@
 package com.github.nayasis.kotlin.basica.thread
 
-import java.beans.PropertyChangeEvent
-import java.beans.PropertyChangeListener
 import java.util.*
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.collections.HashMap
-import kotlin.concurrent.withLock
-import kotlin.reflect.jvm.jvmName
 
-private val EVENT_DELETE = "${ThreadRoot::class.jvmName}.DELETE"
+class NThreadLocal { companion object {
 
-class NThreadLocal {
+    private val pool = Hashtable<String,Hashtable<String,Any?>>()
 
-    companion object {
-
-        private val listeners = mutableListOf<PropertyChangeListener>()
-        private val pool      = HashMap<String,HashMap<String,Any?>>()
-        private val lock      = ReentrantLock()
-        private val condition = lock.newCondition()
-
-        fun addListener(listener: PropertyChangeListener) {
-            listeners.add(listener)
-        }
-
-        operator fun <T> get(key: String): T? {
-            return threadLocal[key] as T?
-        }
-
-        private val threadLocal: MutableMap<String,Any?>
-            get() {
-                lock.withLock {
-                    if (!pool.containsKey(ThreadRoot.getKey())) {
-                        pool[ThreadRoot.getKey()] = HashMap()
-                    }
-                    condition.signalAll()
+    private val threadLocal: MutableMap<String,Any?>
+        get() {
+            synchronized(this) {
+                ThreadRoot.key.let {
+                    if (!pool.containsKey(it))
+                        pool[it] = Hashtable()
+                    return pool[it]!!
                 }
-                return pool[ThreadRoot.getKey()]!!
-            }
-
-        operator fun set(key: String, value: Any?) {
-            threadLocal[key] = value
-        }
-
-        fun remove(key: String) {
-            threadLocal.remove(key)
-        }
-
-        fun containsKey(key: String?): Boolean {
-            return threadLocal.containsKey(key)
-        }
-
-        fun clear() {
-            lock.withLock {
-                pool.remove(ThreadRoot.getKey())
-                condition.signalAll()
-            }
-            listeners.forEach {
-                it.propertyChange(PropertyChangeEvent(this,EVENT_DELETE,true,true))
             }
         }
 
-        fun keySet(): Set<String> = threadLocal.keys
+    operator fun <T> get(key: String): T? = threadLocal[key] as T?
 
-        fun values(): Collection<Any?> = threadLocal.values
-
-        fun getPool(): Map<String,Map<String,Any?>> = pool
-
+    operator fun set(key: String, value: Any?) {
+        threadLocal[key] = value
     }
 
-}
+    fun remove(key: String) = threadLocal.remove(key)
 
+    fun containsKey(key: String): Boolean = threadLocal.containsKey(key)
 
-class ThreadRoot: PropertyChangeListener {
+    fun keySet(): Set<String> = threadLocal.keys
 
-    companion object {
+    fun values(): Collection<Any?> = threadLocal.values
 
-        private val root = object : InheritableThreadLocal<String>() {
-            override fun initialValue(): String = UUID.randomUUID().toString()
-            override fun childValue(parentValue: String): String =
-                if(parentValue.isNullOrEmpty()) initialValue() else parentValue
-        }
-
-        fun getKey(): String = root.get()
-
+    fun clear() {
+        pool.remove(ThreadRoot.key)
+        ThreadRoot.remove()
     }
 
-    override fun propertyChange(event: PropertyChangeEvent) {
-        if( event.propertyName == EVENT_DELETE && event.newValue == true )
-            root.remove()
+    fun pool(): MutableMap<String,Any?> {
+        return threadLocal
     }
 
-    init {
-        NThreadLocal.addListener(ThreadRoot())
+}}
+
+
+class ThreadRoot { companion object {
+
+    private val root = object : InheritableThreadLocal<String>() {
+        override fun initialValue(): String = UUID.randomUUID().toString()
+        override fun childValue(parentValue: String): String =
+            if(parentValue.isNullOrEmpty()) initialValue() else parentValue
     }
 
-}
+    val key: String
+        get() = root.get()
+
+    fun remove() = root.remove()
+
+}}
--- src/test/kotlin/com/github/nayasis/kotlin/basica/thread/NThreadLocalTest.kt
+++ src/test/kotlin/com/github/nayasis/kotlin/basica/thread/NThreadLocalTest.kt
@@ -4,37 +4,69 @@ import mu.KotlinLogging
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.lang.Thread.sleep
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 private val log = KotlinLogging.logger {}
 
 internal class NThreadLocalTest {
 
+    private val threadPool = Executors.newWorkStealingPool(10)
+
     @Test
-    @Throws(InterruptedException::class)
-    fun test() {
+    fun `run in separated threads`() {
 
         val result = HashMap<String, Int?>()
 
         val threadA = makeTestThread("A", 5, result)
         val threadB = makeTestThread("A", 10, result)
 
-        threadA.start()
-        threadB.start()
+        threadPool.execute(threadA)
+        threadPool.execute(threadB)
 
-        sleep(5000)
+        sleep(1000)
+
         assertEquals(10, result["A-10"] as Int)
         assertEquals(5, result["A-5"] as Int)
+        assertNull(NThreadLocal["A"])
 
         log.debug(result.toString())
+
+        NThreadLocal.clear()
+
+    }
+
+    @Test
+    fun `from main thread`() {
+
+        NThreadLocal["A"] = 0
+
+        val result = HashMap<String, Int?>()
+
+        val threadA = makeTestThread("A", 5, result)
+        val threadB = makeTestThread("A", 10, result)
+
+        threadPool.execute(threadA)
+        threadPool.execute(threadB)
+
+        sleep(1000)
+
+        log.debug { NThreadLocal["A"] }
+        assertNotNull(NThreadLocal["A"])
+
+        NThreadLocal.clear()
+        log.debug(result.toString())
+        log.debug { NThreadLocal["A"] }
+        assertNull(NThreadLocal["A"])
 
     }
 
     private fun makeTestThread(key: String, count: Int, result: MutableMap<String, Int?>): Thread {
         return Thread {
             NThreadLocal[key] = 0
-            val child = Thread {
+            threadPool.execute( Thread {
                 for (i in 0 until count) {
-                    var value : Int = NThreadLocal[key]!!
+                    var value : Int = NThreadLocal[key] ?: 0
                     NThreadLocal[key] = ++value
                     log.debug{"key : $key, count : $count, value : $value"}
                     try {
@@ -44,8 +76,7 @@ internal class NThreadLocalTest {
                     }
                 }
                 result["$key-$count"] = NThreadLocal[key]
-            }
-            child.start()
+            })
         }
     }
 
