ae4bbfc6c1aeb3f99979da24ac0a0134a3d6761c
nayasis <nayasis@gmail.com>
2021-07-21 22:11:01 +0900

    extract binder to const

--- src/main/kotlin/com/github/nayasis/kotlin/basica/core/string/format/Formatter.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/basica/core/string/format/Formatter.kt
@@ -17,6 +17,16 @@ val ESCAPE_DETECTOR = { origin: String, matchIndex: Int -> when {
     else -> true
 }}
 
+val DEFAULT_BINDER = { key: BindingKey, param: Map<String, *> ->
+    val value = param[key.name]
+    val exist = param.containsKey(key.name)
+    if (key.format.isEmpty()) {
+        value?.toString() ?: if (exist) null else ""
+    } else {
+        key.format.format(value)
+    }
+}
+
 private val PATTERN_BASIC = ExtractPattern("\\{([^\\s{}]*?)}".toPattern(), ESCAPE_REMOVER, ESCAPE_DETECTOR)
 
 class Formatter {
@@ -37,20 +47,8 @@ class Formatter {
      * @param modifyKorean if true modify first outer character of parameter binding markup by rule of korean.
      * @return formatted string
      */
-    fun bindSimple(format: String, vararg parameter: Any?, modifyKorean: Boolean = true): String {
-        return bind(pattern=PATTERN_BASIC, format=format, binder={ key: BindingKey, param: Map<String, *> ->
-
-            val value = param[key.name]
-            val exist = param.containsKey(key.name)
-
-            if( key.format.isEmpty() ) {
-                value?.toString() ?: if(exist) null else ""
-            } else {
-                key.format.format(value)
-            }
-
-        }, modifyKorean=modifyKorean, parameter=parameter, )
-    }
+    fun bindSimple(format: String, vararg parameter: Any?, modifyKorean: Boolean = true): String =
+        bind(pattern=PATTERN_BASIC, format=format, binder= DEFAULT_BINDER, modifyKorean=modifyKorean, parameter=parameter, )
 
     fun bind(pattern: ExtractPattern, format: String, binder: (key: BindingKey, param: Map<String,*>) -> String?, modifyKorean: Boolean, vararg parameter: Any?,): String {
 
