e4b286e6fed8eeb4d9887fe17231ac4a4c00a1fd
nayasis <nayasis@gmail.com>
2021-08-20 16:40:32 +0900

    update kotlin-logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 	// kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation( "io.github.microutils:kotlin-logging:1.8.3" )
+	implementation("io.github.microutils:kotlin-logging:2.0.10")
 	implementation("au.com.console:kassava:2.1.0")
 
 	// test
