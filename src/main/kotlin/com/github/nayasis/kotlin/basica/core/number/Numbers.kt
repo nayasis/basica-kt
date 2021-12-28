09fa28af5faa774b364dae6182814dfad6ba1057
nayasis <nayasis@gmail.com>
2021-12-28 13:00:39 +0900

    add Duration operator

--- src/main/kotlin/com/github/nayasis/kotlin/basica/core/number/Numbers.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/basica/core/number/Numbers.kt
@@ -3,6 +3,7 @@ package com.github.nayasis.kotlin.basica.core.number
 import java.math.BigDecimal
 import java.math.BigInteger
 import java.text.DecimalFormat
+import java.time.Duration
 import kotlin.reflect.KClass
 
 @Suppress("UNCHECKED_CAST")
@@ -28,3 +29,21 @@ inline fun <reified T:Number> Number.cast(): T {
 fun Number.format(fractionDigits: Int): String {
     return DecimalFormat().apply { maximumFractionDigits = fractionDigits }.format(this)
 }
+
+val Number.millis: Duration
+    get() = Duration.ofMillis(this.toLong())
+
+val Number.seconds: Duration
+    get() = Duration.ofSeconds(this.toLong())
+
+val Number.minutes: Duration
+    get() = Duration.ofMinutes(this.toLong())
+
+val Number.hours: Duration
+    get() = Duration.ofHours(this.toLong())
+
+operator fun Duration.plus(duration: Duration): Duration
+    = this.plus(duration)
+
+operator fun Duration.minus(duration: Duration): Duration
+    = this.minus(duration)
