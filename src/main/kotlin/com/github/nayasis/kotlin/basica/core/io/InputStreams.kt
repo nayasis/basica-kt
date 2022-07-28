7fe6ad8809f2fc0f887de0f3fa7e725ab0c8e3e0
nayasis <nayasis@gmail.com>
2022-07-28 13:43:42 +0900

    indent parameter

--- src/main/kotlin/com/github/nayasis/kotlin/basica/core/io/InputStreams.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/basica/core/io/InputStreams.kt
@@ -3,7 +3,7 @@ package com.github.nayasis.kotlin.basica.core.io
 import java.io.InputStream
 import java.io.OutputStream
 
-fun InputStream.copyTo(out: OutputStream,bufferSize: Int = DEFAULT_BUFFER_SIZE,callback: ((copiedSize: Long) -> Unit)?): Long {
+fun InputStream.copyTo(out: OutputStream, bufferSize: Int = DEFAULT_BUFFER_SIZE, callback: ((copiedSize: Long) -> Unit)?): Long {
     var copied = 0L
     val buffer = ByteArray(bufferSize)
     var bytes = read(buffer)
