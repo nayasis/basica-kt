f02396a68e1b9a636c6eaf166e525876d76fce45
nayasis <nayasis@gmail.com>
2023-04-27 14:21:10 +0900

    refactoring type casting function

--- src/main/kotlin/com/github/nayasis/kotlin/basica/core/validator/Types.kt
+++ src/main/kotlin/com/github/nayasis/kotlin/basica/core/validator/Types.kt
@@ -1,21 +1,71 @@
 package com.github.nayasis.kotlin.basica.core.validator
 
+import com.github.nayasis.kotlin.basica.core.klass.isEnum
+import com.github.nayasis.kotlin.basica.core.klass.isPrimitive
+import com.github.nayasis.kotlin.basica.core.number.cast
+import com.github.nayasis.kotlin.basica.core.string.toNumber
+import com.github.nayasis.kotlin.basica.reflection.Reflector
+import java.io.File
+import java.net.URI
+import java.net.URL
+import java.nio.file.Path
+import java.time.temporal.Temporal
+import java.util.*
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+
 class Types { companion object {
 
     fun isPojo(any: Any?): Boolean {
         if( any == null ) return false
+        val klass = any::class
+        return when {
+            klass.isPrimitive -> false
+            klass.java.isAnnotation -> false
+            klass.isEnum -> false
+            klass.isValue -> false
+            klass.isData -> true
+            any is Boolean -> false
+            any is CharSequence -> false
+            any is Char -> false
+            any is Number -> false
+            any is Date -> false
+            any is Calendar -> false
+            any is Temporal -> false
+            any is URI -> false
+            any is URL -> false
+            any is File -> false
+            any is Path -> false
+            else -> true
+        }
+    }
 
-        val klass = any.javaClass
+    @Suppress("UNCHECKED_CAST")
+    fun <T: Any> cast(value: Any?, typeClass: KClass<T>, ignoreError: Boolean = true): T? {
+        return when {
+            value == null -> null
+            value::class.isSubclassOf(typeClass) -> value as T
+            typeClass == String::class -> value.toString() as T
+            (value is CharSequence || value is Char) && typeClass.isSubclassOf(Number::class) ->
+                value.toString().toNumber(typeClass as KClass<Number>) as T
+            value is Number ->
+                value.cast(typeClass as KClass<Number>) as T
+            else -> {
+                try {
+                    Reflector.toObject(value, typeClass)
+                } catch (e1: Exception) {
+                    if( ignoreError ) {
+                        null
+                    } else {
+                        throw ClassCastException("Value($value) cannot be cast to ${typeClass.simpleName}")
+                    }
+                }
+            }
+        }
+    }
 
-        if( klass.isPrimitive ) return false
-        if( klass.isAnnotation ) return false
-        if( klass.isEnum ) return false
-
-        if( any is CharSequence ) return false
-        if( any is Char ) return false
-
-        return true
-
+    inline fun <reified T: Any> cast(value: Any?, ignoreError: Boolean = true): T? {
+        return cast(value, T::class, ignoreError)
     }
 
 }}
---
+++ src/test/kotlin/com/github/nayasis/kotlin/basica/core/validator/TypesTest.kt
@@ -0,0 +1,71 @@
+package com.github.nayasis.kotlin.basica.core.validator
+
+import com.github.nayasis.kotlin.basica.core.io.Path
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.net.URI
+import java.net.URL
+import java.time.LocalDateTime
+import java.util.Calendar
+import java.util.Date
+
+class TypesTest {
+
+    @Test
+    fun isPojo() {
+        assertEquals(true, Types.isPojo(TestDataClass("nobody")))
+        assertEquals(true, Types.isPojo(TestNormalClass("nobody")))
+        assertEquals(false, Types.isPojo("nobody"))
+        assertEquals(false, Types.isPojo(StringBuffer("nobody")))
+        assertEquals(false, Types.isPojo(StringBuilder("nobody")))
+        assertEquals(false, Types.isPojo(1.toByte()))
+        assertEquals(false, Types.isPojo(true))
+        assertEquals(false, Types.isPojo(false))
+        assertEquals(false, Types.isPojo(0.1))
+        assertEquals(false, Types.isPojo(0.1F))
+        assertEquals(false, Types.isPojo(1))
+        assertEquals(false, Types.isPojo(1L))
+        assertEquals(false, Types.isPojo(BigDecimal.valueOf(1)))
+        assertEquals(false, Types.isPojo(BigInteger.valueOf(1)))
+        assertEquals(false, Types.isPojo(LocalDateTime.now()))
+        assertEquals(false, Types.isPojo(Date()))
+        assertEquals(false, Types.isPojo(Calendar.getInstance()))
+        assertEquals(false, Types.isPojo(File("/a/b")))
+        assertEquals(false, Types.isPojo(Path("/a/b")))
+        assertEquals(false, Types.isPojo(URI("/a/b")))
+        assertEquals(false, Types.isPojo(URL("https://www.google.com")))
+    }
+
+    @Test
+    fun cast() {
+
+        assertEquals("A", Types.cast<String>("A"))
+        assertEquals(1, Types.cast(1))
+        assertEquals(1.0, Types.cast(1.0))
+        assertEquals(1.0F, Types.cast(1.0F))
+
+        assertEquals(1, Types.cast(1.0))
+        assertEquals(1, Types.cast(1.0F))
+        assertEquals(1, Types.cast(1L))
+        assertEquals(1, Types.cast(BigDecimal.valueOf(1)))
+        assertEquals(1, Types.cast(BigInteger.valueOf(1)))
+        assertEquals(1, Types.cast("1"))
+        assertEquals(1, Types.cast("1.0"))
+
+        assertEquals("1", Types.cast<String>(1))
+        assertEquals("1", Types.cast<String>(BigDecimal.valueOf(1)))
+        assertEquals("1", Types.cast<String>(BigInteger.valueOf(1)))
+    }
+
+}
+
+data class TestDataClass(
+    val name: String
+)
+
+class TestNormalClass(
+    val name: String
+)
