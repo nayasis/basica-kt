d8fe62dc8e38c69f02cab8e96485cbbd26740a63
nayasis <nayasis@gmail.com>
2023-08-02 18:31:08 +0900

    remove dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 	`maven-publish`
 	kotlin("jvm") version "1.8.10"
 	kotlin("plugin.noarg") version "1.8.10"
-	id("org.jetbrains.dokka") version "1.8.10"
 }
 
 noArg {
@@ -32,16 +31,14 @@ java {
 repositories {
 	mavenLocal()
 	mavenCentral()
-	jcenter()
-	maven { url = uri("https://jitpack.io") }
 }
-8888
+
 dependencies {
 
-	implementation("org.mvel:mvel2:2.4.12.Final")
+	implementation("org.mvel:mvel2:2.4.15.Final")
 	implementation("com.googlecode.juniversalchardet:juniversalchardet:1.0.3")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.+")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.2")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.2")
 
 	"supportImplementation"("ch.qos.logback:logback-classic:1.3.5")
 
@@ -49,12 +46,16 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.github.microutils:kotlin-logging:3.0.5")
-	implementation("au.com.console:kassava:2.1.0")
+//	implementation("au.com.console:kassava:2.0.0")
 
 	// test
-	testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+	testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+
+	testImplementation("io.kotest:kotest-runner-junit5:5.6.1")
+	testImplementation("io.kotest:kotest-assertions-core:5.6.1")
+
 	testImplementation("ch.qos.logback:logback-classic:1.3.5")
 
 }
