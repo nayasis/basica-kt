4d9b38a580c468bf23533f636099cbd815f5415c
nayasis <nayasis@gmail.com>
2025-11-26 18:25:21 +0900

    remove debug print statement for mavenReleaseVersion in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ plugins {
 group = "io.github.nayasis"
 version = when {
     project.hasProperty("mavenReleaseVersion") && project.property("mavenReleaseVersion") != "unspecified" && project.property("mavenReleaseVersion") != "" -> {
-        println("\t- mavenReleaseVersion: ${project.property("mavenReleaseVersion")}")
         project.property("mavenReleaseVersion") as String
     }
     else -> "0.1.0-SNAPSHOT"
